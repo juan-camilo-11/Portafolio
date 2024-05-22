b8f37df69ea5d46e30db76808207a653be5ee59f
juan-camilo-11 <thekmilo84@gmail.com>
2024-05-22 12:37:49 -0500

    Agrego pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-    integrations: [tailwind()],
-  });
+  site: 'https://juan-camilo-11.github.io',
+  integrations: [tailwind()],
+});
